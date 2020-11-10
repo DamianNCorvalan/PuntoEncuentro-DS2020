4cd502288782b01d9bc306af4f9bb609436e0570
José Gómez <josecg_86@hotmail.com>
2020-11-09 21:21:13 -0300

    Registro Usuario 2

--- PuntoEncuento/Controllers/AccountController.cs
+++ PuntoEncuento/Controllers/AccountController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using PuntoEncuento.Models;
+using System.Data.Entity.Validation;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -377,6 +379,7 @@ namespace PuntoEncuento.Controllers
                     usuario.Domicilio.Localidad.Partido = db.Partido.FirstOrDefault(x => x.IdPartido == Usuario.IdPartido);
                     usuario.Domicilio.Localidad.Partido.Provincia = db.Provincia.FirstOrDefault(x => x.IdProvincia == Usuario.IdProvincia);
                     usuario.Rol = db.Rol.FirstOrDefault(x => x.IdRol == Usuario.IdRol);
+                    usuario.CorreoElectronico = model.Email;
                     //if (file != null && file.ContentLength > 0)
                     //    try
                     //    {
@@ -393,11 +396,30 @@ namespace PuntoEncuento.Controllers
                     //    {
                     //        //ViewBag.Message = "ERROR:" + ex.Message.ToString();
                     //    }
-
                     db.Usuario.Add(usuario);
-                    db.SaveChanges();
+                    //db.SaveChanges();
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        StringBuilder sb = new StringBuilder();
 
-
+                        foreach (var failure in ex.EntityValidationErrors)
+                        {
+                            sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                            foreach (var error in failure.ValidationErrors)
+                            {
+                                sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                                sb.AppendLine();
+                            }
+                        }
+                        throw new DbEntityValidationException(
+                            "Entity Validation Failed - errors follow:\n" +
+                            sb.ToString(), ex
+                        ); // Add the original exception as the innerException
+                    }
                     result = await UserManager.AddLoginAsync(user.Id, info.Login);
                     if (result.Succeeded)
                     {
--- PuntoEncuento/Models/IdentityModels.cs
+++ PuntoEncuento/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace PuntoEncuento.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("PuntoEncuentro", throwIfV1Schema: false)
         {
         }
 
